30140c58dabcd72512e3cdf948e4a7acd3cece65
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-18 23:20:37 +0500

    add these files
    we add in Link onlu keys._productId which gets the index numer and we can extract whole object or array via index nub

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import products from '../../Data/data.json'
+import {products} from '../../Data/data.json'
 import {useParams} from 'react-router-dom'
 
 
@@ -7,7 +7,9 @@ export const Cart = () => {
     // console.log(products)
 
     const {id} = useParams()
+    console.log("ID FROM CART: ", id);
     const shoe=products[id]
+    console.log(shoe)
 
 
 if(!shoe){
@@ -18,8 +20,8 @@ if(!shoe){
         <div>
             <h1>we are on cart</h1>
             <h2>{shoe.name}</h2>
-            <img src={shoe.img} alt="images"  height="500px" style={{}}/>
-            <h2>{shoe.price}</h2>
+            <img src={shoe.image} alt="images"  height="500px" style={{}}/>
+            <h2>${shoe.price}</h2>
         </div>
     )
 }
--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import shoes  from '../../Data/data.json'
-// import { Grid, Card, CardContent, Typography,CardActionArea,CardMedia,Button,CardActions } from '@material-ui/core';
+import {products}  from '../../Data/data.json'
+import { Grid, Card, CardContent, Typography,CardActionArea,CardMedia,Button,CardActions } from '@material-ui/core';
 
 import styles from './Product.module.css'
 export const Product = () => {
@@ -11,33 +11,17 @@ export const Product = () => {
         
         <div>
            <h1 className={styles.heading}>PRODUCT</h1> 
-       <ul>
-        {Object.keys(shoes).map((keyName) =>{
-          const shoe=shoes[keyName]
-          console.log(shoe)
-          return(
-            <div>
-              <Link key={keyName} to={`/product/${keyName}`}>
-                <h1>{shoe.name}</h1>
-                <img src={shoe.img} alt="images"/>
-                <br/>
-                <h1>{shoe.price}</h1>
-              </Link>
-            </div>
-          )
-        })}
-       </ul>
  
-            {/* <Grid container className={styles.container}>
+            <Grid container className={styles.container}>
         {products.map((keys) =>{ return(
 
                 <Grid item xs={12} sm={6} md={4} key={keys._productId}>
-                <Card> */}
+                <Card>
+                <Link key={keys._productId} to={`/product/${keys._productId}`}>
 
-      {/* <CardActionArea> */}
-      {/* <Link key={keys._productId} to={`/product/${keys._productId,keys.name,keys.price,keys.image}`}> */}
+      <CardActionArea>
 
-        {/* <CardMedia
+        <CardMedia
           component="img"
           alt="Contemplative Reptile"
           height="140"
@@ -53,22 +37,22 @@ export const Product = () => {
             ${keys.price}
           </Typography>
 
-          </CardContent> */}
-          {/* </Link> */}
+          </CardContent>
+          
 
-     {/* </CardActionArea>
+     </CardActionArea>
       <CardActions>
         <Button size="small" color="primary" className={styles.cartbtn}>
             Add To Cart          
         </Button>
        
       </CardActions>
-
+      </Link>
     </Card>
     
                 </Grid>
         )})}
-            </Grid> */}
+            </Grid>
         </div>
     )
 }
